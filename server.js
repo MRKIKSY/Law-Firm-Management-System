cfd212cb7aa43f6096862f8c4357f14f56dfe430
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-09-18 06:34:31 +0100

    now

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5000"],
+  origin: ["https://frontend-law-firm-database.onrender.com","http://localhost:5173","http://localhost:5000"],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
