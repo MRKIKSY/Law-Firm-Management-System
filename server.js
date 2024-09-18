4e5a853d5c7f1283d9490f43053094ce8ca3d3ef
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2024-09-18 06:33:25 +0100

    cors changes

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173","http://localhost:5000"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
+
+
+
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(path.resolve(), 'uploads')));
 
